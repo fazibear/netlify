cfa6519b4360be611ced43a076bacae5215e45de
Michał Kalbarczyk <fazibear@gmail.com>
2015-10-25 19:55:18 +0100

    init app

---
+++ app/components/home.rb
@@ -0,0 +1,15 @@
+class Home
+  include Inesita::Component
+
+  def render
+    div class: 'jumbotron text-center' do
+      img src: 'http://avatars.githubusercontent.com/inesita-rb'
+      h1 do
+        text "Hello I'm Inesita"
+      end
+      h4 do
+        text 'This is a sample component'
+      end
+    end
+  end
+end
